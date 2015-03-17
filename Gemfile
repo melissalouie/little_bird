db6115f8cafbb1cf21044736b272e6f246dfa198
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-03-17 14:11:27 -0600

    add navbar and styled form on index

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'dotenv-rails'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'foundation-rails'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).foundation();
+
+$(function(){ $(document).foundation(); });
