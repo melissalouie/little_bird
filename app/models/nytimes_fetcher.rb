40648a56a62ca40cd41ab2dcc9238c2c38e0e365
Ty Hyten <ty.hyten@gmail.com>
2015-03-19 11:34:20 -0600

    article index page consistently styled

--- app/models/nytimes_fetcher.rb
+++ app/models/nytimes_fetcher.rb
@@ -12,21 +12,20 @@ class NytimesFetcher
     response = @nytimes_connection.get do |req|
       req.url "/svc/search/v2/articlesearch.json?fq=section_name:(#{section})"
       req.params['api-key'] = ENV["NYTIMES_KEY"]
-
+      # req.params['page'] = 2
       req.headers['Content-Type'] = 'application/json'
     end
     JSON.parse(response.body)
   end
 
+
   def article(_id)
     response = @nytimes_connection.get do |req|
       req.url "/svc/search/v2/articlesearch.json?fq=_id:(#{_id})"
       req.params['api-key'] = ENV["NYTIMES_KEY"]
-
       req.headers['Content-Type'] = 'application/json'
     end
     JSON.parse(response.body)
-
   end
 
   def nytimes_comments(web_url)
